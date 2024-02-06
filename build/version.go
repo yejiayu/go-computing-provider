676e7c170ba5f326813cd6853e4dde2277879bd5
sonic <longzhang>
2024-02-06 17:40:19 +0800

    update version

--- build/version.go
+++ build/version.go
@@ -2,14 +2,12 @@ package build
 
 var CurrentCommit string
 
-const BuildVersion = "0.4.2"
+const BuildVersion = "0.4.3"
 
 const UBITaskImageIntelCpu = "filswan/ubi-worker-cpu-intel:v2.0"
 const UBITaskImageIntelGpu = "filswan/ubi-worker-gpu-intel:v2.0"
 const UBITaskImageAmdCpu = "filswan/ubi-worker-cpu-amd:v2.0"
 const UBITaskImageAmdGpu = "filswan/ubi-worker-gpu-amd:v2.0"
-
-const CpuCollectorImage = "filswan/cpu-model-collector:v1.0"
 
 func UserVersion() string {
 	return BuildVersion + CurrentCommit
