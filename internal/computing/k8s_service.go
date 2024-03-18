c53c3ad5ec83770de0ea2019d4819004f41c1bfa
sonic <longzhang>
2024-03-18 19:49:28 +0800

    modify report resource

--- internal/computing/k8s_service.go
+++ internal/computing/k8s_service.go
@@ -530,6 +530,9 @@ func (s *K8sService) WaitForPodRunningByTcp(namespace, taskUuid string) (string,
 				return false, nil
 			}
 		}
+		if len(podList.Items) > 0 {
+			return false, nil
+		}
 		podName = podList.Items[0].Name
 		return true, nil
 	})
