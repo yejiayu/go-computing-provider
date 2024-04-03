ea669fa7c44cc210fd0d40a9336e01bea4aaf5b7
sonic <longzhang>
2024-04-03 18:48:10 +0800

    add log

--- internal/computing/docker_service.go
+++ internal/computing/docker_service.go
@@ -342,6 +342,7 @@ func (ds *DockerService) ContainerLogs(containerName string) (string, error) {
 	ctx := context.Background()
 	logReader, err := ds.c.ContainerLogs(ctx, containerName, types.ContainerLogsOptions{
 		ShowStdout: true,
+		Follow:     true,
 		Tail:       "1",
 	})
 	if err != nil {
