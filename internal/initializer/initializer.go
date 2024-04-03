8f92b13d68de7fc6c3a01c928456724a92748824
sonic <longzhang>
2024-04-03 17:55:08 +0800

    add log

--- internal/initializer/initializer.go
+++ internal/initializer/initializer.go
@@ -36,9 +36,9 @@ func sendHeartbeat(nodeId string) {
 		logs.GetLogger().Errorf("Error sending heartbeat, retrying to connect to the Swan Hub server: %v", err)
 		computing.Reconnect(nodeId)
 	} else {
-		_, err := ioutil.ReadAll(resp.Body)
+		data, err := ioutil.ReadAll(resp.Body)
 		if resp.StatusCode != http.StatusOK {
-			logs.GetLogger().Warningln("resp status: %d, retrying to connect to the Swan Hub server", resp.StatusCode)
+			logs.GetLogger().Warningln("resp status:", resp.StatusCode, "error:", string(data), "retrying to connect to the Swan Hub server")
 			computing.Reconnect(nodeId)
 		}
 		if err != nil {
