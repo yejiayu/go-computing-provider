166ca95977936762cf1d16fd72e53fdcd167c64e
sonic <longzhang>
2024-04-16 18:09:39 +0800

    code review

--- constants/utils.go
+++ constants/utils.go
@@ -15,7 +15,6 @@ const TASK_DEPLOY string = "worker.deploy"
 
 const K8S_NAMESPACE_NAME_PREFIX = "ns-"
 const K8S_CONTAINER_NAME_PREFIX = "pod-"
-const K8S_PRIVATE_CONTAINER_PREFIX = "vm-"
 const K8S_INGRESS_NAME_PREFIX = "ing-"
 const K8S_SERVICE_NAME_PREFIX = "svc-"
 const K8S_DEPLOY_NAME_PREFIX = "deploy-"
