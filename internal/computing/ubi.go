4f79bcb3d9b4dc6f4c7cbaa7b4ed4512a289a97b
sonic <longzhang>
2024-04-28 17:06:39 +0800

    add log

--- internal/computing/ubi.go
+++ internal/computing/ubi.go
@@ -625,7 +625,6 @@ func DoUbiTaskForDocker(c *gin.Context) {
 					{
 						Driver:       "nvidia",
 						Count:        -1,
-						DeviceIDs:    []string{"0", "1", "2", "3", "4", "5", "6", "7"},
 						Capabilities: [][]string{{"gpu"}},
 						Options:      nil,
 					},
