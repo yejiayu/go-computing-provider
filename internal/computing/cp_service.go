8bf456bf7d6d9bafb1a22aca56b45013fd0e3942
sonic <longzhang>
2024-05-16 18:37:17 +0800

    update check gpu name

--- internal/computing/cp_service.go
+++ internal/computing/cp_service.go
@@ -1005,7 +1005,7 @@ func checkResourceAvailableForUbi(taskType int, gpuName string, resource *models
 					continue
 				}
 				gpuName = strings.ReplaceAll(gpuName, " ", "-")
-				logs.GetLogger().Infof("gpuName: %s, nodeGpu: %+v, nodeGpuSummary: %+v", gpuName, nodeGpu, nodeGpuSummary)
+				logs.GetLogger().Infof("needGpuName: %s, nodeGpu: %+v, nodeGpuSummary: %+v", gpuName, nodeGpu, nodeGpuSummary)
 				usedCount, ok := nodeGpu[gpuName]
 				if !ok {
 					usedCount = 0
@@ -1331,8 +1331,14 @@ func convertGpuName(name string) string {
 			return strings.Replace(name, "Tesla ", "", 1)
 		}
 
-		if strings.Contains(name, "GeForce") {
-			name = strings.Replace(name, "GeForce ", "", 1)
+		if strings.HasPrefix(strings.ToUpper(name), "NVIDIA") {
+			if strings.Contains(name, "GeForce") {
+				name = strings.Replace(name, "GeForce", "", 1)
+			}
+		} else {
+			if strings.Contains(name, "GeForce") {
+				name = strings.Replace(name, "GeForce ", "", 1)
+			}
 		}
 		return strings.Replace(name, "RTX ", "", 1)
 	} else {
