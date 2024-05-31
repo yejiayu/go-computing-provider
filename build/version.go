47f76cecf283b392217313e79a42c54a6a02538a
sonic <longzhang>
2024-05-31 16:46:24 +0800

    update hardware-exporter version

--- build/version.go
+++ build/version.go
@@ -8,7 +8,7 @@ const UBITaskImageIntelCpu = "filswan/ubi-worker-cpu-intel:v2.0"
 const UBITaskImageIntelGpu = "filswan/ubi-worker-gpu-intel:v2.0"
 const UBITaskImageAmdCpu = "filswan/ubi-worker-cpu-amd:v2.0"
 const UBITaskImageAmdGpu = "filswan/ubi-worker-gpu-amd:v2.0"
-const UBIResourceExporterDockerImage = "filswan/hardware-exporter:v1.0"
+const UBIResourceExporterDockerImage = "filswan/hardware-exporter:v2.0"
 
 func UserVersion() string {
 	return BuildVersion + CurrentCommit
